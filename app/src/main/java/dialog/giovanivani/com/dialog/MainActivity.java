a95998609dc46ca31cd141d4b8036547295cb799
Giovani Oliveira <Giovani@MacBook-Pro-de-Giovani.local>
2016-12-09 01:01:05 -0400

    Initial commit

---
+++ app/src/main/java/dialog/giovanivani/com/dialog/MainActivity.java
@@ -0,0 +1,26 @@
+package dialog.giovanivani.com.dialog;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    private Button botao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botao = (Button) findViewById(R.id.botaoId);
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
